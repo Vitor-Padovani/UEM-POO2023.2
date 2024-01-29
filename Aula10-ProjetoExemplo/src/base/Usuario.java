4db8cf25f7f89c74a388b244dc75ad209917e3c4
igornatalunicesumar <150091115+igornatalunicesumar@users.noreply.github.com>
2024-01-29 13:44:48 -0300

    V0.1
    Tela Login
    Tela Inicial
    Tela de Cadastro de Usuário

---
+++ Aula10-ProjetoExemplo/src/base/Usuario.java
@@ -0,0 +1,54 @@
+package base;
+
+public class Usuario {
+    private String nome, email, senha, usuario, dtNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getDtNascimento() {
+        return dtNascimento;
+    }
+
+    public void setDtNascimento(String dtNascimento) {
+        this.dtNascimento = dtNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nome=" + nome + ", email=" + email + ", senha=" + senha + ", usuario=" + usuario + ", dtNascimento=" + dtNascimento + '}';
+    }
+    
+    
+    
+    
+}
